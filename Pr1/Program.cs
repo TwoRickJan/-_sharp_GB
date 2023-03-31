207aaffee5d7c2fc328b2f1fc90909f2c9cbfef9
Arthur <artyrzab@gmail.com>
2023-03-31 19:02:47 +0500

    add pr1 pr2

--- Pr1/Program.cs
+++ Pr1/Program.cs
@@ -1,2 +1,87 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задачf 2: Напишите программу, которая на вход принимает
+ два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+int max = Convert.ToInt32(Console.ReadLine());
+int min = Convert.ToInt32(Console.ReadLine());
+int v;
+if(max<min)
+{
+    v = max;
+    max = min;
+    min = v;
+}
+Console.WriteLine($"max -> {max}",max);
+
+/*
+Задача 4: Напишите программу, которая принимает 
+на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+
+*/
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int maxx = a;
+
+if(maxx<b)
+{
+    maxx = b;
+};
+if(maxx<c)
+{
+    maxx = c;
+};
+
+Console.WriteLine($"{a},{b},{c} -> {maxx}",a,b,c,maxx);
+
+
+/*
+Задача 6: Напишите программу, которая на вход принимает число
+и выдаёт, является ли число чётным (делится ли оно на два 
+без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+
+int num6 = Convert.ToInt32(Console.ReadLine());
+if(num6 % 2==0)
+{
+Console.WriteLine($"{num6} -> да",num6);
+}
+else
+{
+Console.WriteLine($"{num6} -> нет",num6);
+}
+
+/*
+Задача 8: Напишите программу, которая на 
+вход принимает число (N), а на выходе показывает
+ все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+int aa = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= aa; i++)
+{
+    if (i%2==0)
+    {
+        Console.Write(i);
+        Console.Write(", ");
+    };
+};
+
--- Pr2/Program.cs
+++ Pr2/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+/*
+Задача 10: Напишите программу, которая принимает на 
+вход трёхзначное число и на выходе показывает 
+вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(num%100/10);
+
+
+/*
+Задача 13: Напишите программу, которая выводит третью 
+цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+int num13 = Convert.ToInt32(Console.ReadLine());
+if(num13%100!=num13)
+{
+    int s=num13;
+    while(s%1000!=s)
+    {
+        s=s/10;
+    };
+    Console.WriteLine(s%10);
+}
+else
+{
+    Console.WriteLine("третей цифры нет");
+};
+
+/*
+Задача 15: Напишите программу, которая принимает на вход 
+цифру, обозначающую день недели, и проверяет, является ли
+этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+int num15 = Convert.ToInt32(Console.ReadLine());
+
+if((num15==6)|(num15==7))
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.Write("нет");
+}
